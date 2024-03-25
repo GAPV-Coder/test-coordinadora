d81f667cd5b6ad03692e2ea7d31c2f1b9916e671
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2024-03-25 11:19:19 -0500

    feat: Add configuration for hourly request limit
    Also add security configuration with helmet

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import config from './config.js';
 import sequelize from './database/database.js';
 import routes from './routes/index.js';
@@ -28,6 +30,16 @@ app.use((_req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(helmet());
+
+// Limit hourly requests
+const limiter = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests from this IP, please try again later.'
+});
+
+app.use(limiter);
 
 // Database connection
 sequelize
